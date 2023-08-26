883c5217be5953cba7243eb9881de908b24beb4f
nqbao47 <baonguyengau14@gmail.com>
2023-08-26 10:00:28 +0700

    5 [Add] Layout for Homepage

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import OcrPage from "./pages/ocrPage";
+import HomePage from "./pages/homePage";
 
 export default function AppRouter() {
-
-    return (
-        <></>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/ocrpage"
+          element={<OcrPage />}
+        />
+      </Routes>
+    </Router>
+  );
 }
